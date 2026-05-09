b687d1960df0b87e87475fd55b7f2b641e90bc43
omega-div913 <omega.k090103@gmail.com>
2026-05-09 16:08:24 +0530

    initial commit - production

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() { 
+        return Product::all(); 
+    }
+
+    public function store(Request $request) {
+        return Product::create($request->all());
+    }
+
+    public function update(Request $request, $id) {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy($id) {
+        return Product::destroy($id);
+    }
+}
